3c9d62e5e14b9588b58463427afb7975ff9bbf5f
Joe Walnes <joe@walnes.com>
2016-03-05 23:38:53 -0600

    First pass at --watch

--- bin/crunch.js
+++ bin/crunch.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 'use strict'
 
-const crunch = require('../lib/crunch.js'),
+const chokidar = require('chokidar'),
+      crunch = require('../lib/crunch.js'),
       yargs = require('yargs')
 
 const argv = yargs
@@ -25,6 +26,11 @@ const argv = yargs
         describe: 'debug build (does not minify output)',
         type: 'boolean',
       },
+      'watch': {
+        alias: 'w',
+        describe: 'keep running and rebuild whenever input files change',
+        type: 'boolean',
+      },
     })
     .help('help')
     .alias('h', 'help')
@@ -32,17 +38,59 @@ const argv = yargs
     .epilog('https://github.com/joewalnes/crunchicorn')
     .argv
 
-crunch.crunchJs(argv._[0], argv._[1], {
-  banner: argv.banner,
-  css: true,
-  sourceMap: !argv.nomap,
-  minifyCss: !argv.debug,
-  minifyJs: !argv.debug,
-  fontMagician: !argv.nofontface,
-  resolve: true,
-}).then(result => {
-  //console.log('SUCCESS', result)
-}).catch(err => {
-  console.error('ERROR', err)
-  process.exit(1)
-})
+const src = argv._[0],
+      dest = argv._[1],
+      opts = {
+        banner: argv.banner,
+        css: true,
+        sourceMap: !argv.nomap,
+        minifyCss: !argv.debug,
+        minifyJs: !argv.debug,
+        fontMagician: !argv.nofontface,
+        resolve: true,
+      }
+
+if (argv.watch) {
+
+  let deps = [src]
+  let watcher = chokidar.watch(deps, {})
+
+  function updateDeps(newDeps) {
+    for (let dep of newDeps) {
+      if (deps.indexOf(dep) == -1) {
+        //console.log('+ watch', dep)
+        watcher.add(dep)
+      }
+    }
+    for (let dep of deps) {
+      if (newDeps.indexOf(dep) == -1) {
+        // console.log('- watch', dep)
+        watcher.unwatch(dep)
+      }
+    }
+    deps = newDeps
+  }
+
+  function doCrunch() {
+    process.stdout.write(`! building ${dest} ... `)
+    crunch.crunchJs(src, dest, opts)
+      .then(result => {
+        console.log('OK')
+        updateDeps(result.deps)
+      })
+      // TODO: how to handle errors?
+      // See https://github.com/rollup/rollup/issues/545
+      .catch(err => console.log('ERROR', err))
+  }
+
+  watcher.on('change', doCrunch)
+  doCrunch()
+
+} else {
+
+  crunch.crunchJs(src, dest, opts).catch(err => {
+    console.error('ERROR', err)
+    process.exit(1)
+  })
+
+}
